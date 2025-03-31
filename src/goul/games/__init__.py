b932c2faa639e3f46bee453efb977ce52ca2899b
Hideo Kuze <sulyi.gbox@gmail.com>
2025-03-31 10:35:48 +0200

    Fix code-style

--- src/goul/games/__init__.py
+++ src/goul/games/__init__.py
@@ -2,15 +2,15 @@ from .game1 import Game1
 from .game2 import Game2
 
 _games = [Game1, Game2]
-
 _game_lookup = {game.name(): game for game in _games}
+
 
 def get_game_names():
     return [game.name() for game in _games]
 
 
 def from_game_name(name, state=None):
-        try:
-            return _game_lookup[name](state)
-        except KeyError as error:
-            raise KeyError(f"No game named \"{name}\"") from error
+    try:
+        return _game_lookup[name](state)
+    except KeyError as error:
+        raise KeyError(f"No game named \"{name}\"") from error

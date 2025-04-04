28debef82e49ac123f201f8f80f3e87db6cf557d
Hideo Kuze <sulyi.gbox@gmail.com>
2025-04-04 15:37:29 +0200

    Refactor GameState

--- src/goul/games/game2.py
+++ src/goul/games/game2.py
@@ -10,17 +10,15 @@ class Game2(GameBase):
     _meta["imshow"].update({"origin": "lower"})
 
     _n_data = 50
-    _xdata = list(range(_n_data))
 
     def __init__(self, state):
         super().__init__(state)
         self._count = 0
 
     def __next__(self):
-        self.state.ydata = [self._count for _ in range(self._n_data)]
+        self.state.cells = {(x, self._count) for x, _ in self.state.cells}
         self._count += 1
         return self.state
 
     def get_init_state(self):
-        ydata = [0 for _ in range(self._n_data)]
-        return GameState(self._xdata, ydata)
+        return GameState({(x, 0) for x in range(self._n_data)})
--- src/goul/games/game_state.py
+++ src/goul/games/game_state.py
@@ -1,24 +1,21 @@
 from dataclasses import dataclass
-from typing import List
-
 
 @dataclass
 class GameState:
-    xdata: List[int]
-    ydata: List[int]
+    cells: set[tuple[int, int]]
 
     @property
     def data(self):
         # Convert sparse data to binary matrix
-        min_x = min(self.xdata)
-        min_y = min(self.ydata)
+        min_x = min(x for x, _ in self.cells)
+        min_y = min(y for _, y in self.cells)
 
-        width = max(self.xdata) - min_x + 1
-        height = max(self.ydata) - min_y + 3
+        width = max(x for x, _ in self.cells) - min_x + 1
+        height = max(y for _, y in self.cells) - min_y + 3
 
         data = [[0 for _ in range(width)] for _ in range(height)]
 
-        for x, y in zip(self.xdata, self.ydata):
+        for x, y in self.cells:
             data[y - min_y + 1][x - min_x] = 1
 
         return data
